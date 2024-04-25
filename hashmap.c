36d43d7b751cdce7da5ce449bef8a4034755cdbb
JoaquinGarridoRiquelme <163446654+JoaquinGarridoRiquelme@users.noreply.github.com>
2024-04-25 03:09:15 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,7 +40,7 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-  if((float(map->size) / float(map->capacity)) > 0.7)
+  if((float(map->size) / float(map->capacity)) >= 0.7)
     enlarge(map);
   long pos = hash(key, map->capacity);
   while(map->buckets[pos] != NULL && map-> buckets[pos] -> key != NULL)
